79b70e0e5c4bc4dac37afabeac7aec752f38977f
yousefjoe1 <yousefmahmoud150@gmail.com>
2024-08-03 22:14:13 +0300

    add git init

--- src/comp/Nav/Navbar.tsx
+++ src/comp/Nav/Navbar.tsx
@@ -115,31 +115,31 @@ useEffect(() => {
 
   return (
     <nav style={{height:'100vh'}} className='flex items-center relative justify-center'>
-      <div className={`${st.pointer_link} ${showboxLinks} ${boxPosition} absolute z-50`}>
+      {/* <div className={`${st.pointer_link} ${showboxLinks} ${boxPosition} absolute z-50`}>
         <span className='arrow-pointer'><AiFillCaretDown className='arrow-pointer-animation' style={{fontSize:'35px'}} /></span>
-      </div>
+      </div> */}
 
       <div className={`${st.navLinks_wrapper} ${navWrapperActive} relative z-10 ${rotateval}`}>
       <div className="absolute z-0 w-full h-full rounded-full bg-black/20" />
 
         <div className={`${st.skills} skills-div relative z-50`}>
-          <a href="" className='' onClick={(e)=> rotationFunc('skills',e)}>Skills</a>
+      <a href="" className={`${rotateval == 'skills-rotate'? 'border-b-2':''}`} onClick={(e)=> rotationFunc('skills',e)}>Skills</a>
         </div>
 
         <div className={`${st.about_certificates} overflow-hidden relative z-50`}>
 
           <div className={`${st.about} about-div`}>
-            <a className='text-center block w-full' onClick={(e)=> rotationFunc('about',e)} href="">About</a>
+            <a className={`${rotateval == 'about-rotate'? 'border-b-2':''} text-center block w-full`} onClick={(e)=> rotationFunc('about',e)} href="">About</a>
           </div>
 
           <div className={`${st.certificates} certificate-div`}>
-            <a className='text-center block w-full' onClick={(e)=> rotationFunc('certificates',e)} href="">Certificates</a>
+            <a className={`${rotateval == 'certificate-rotate-from-about'? 'border-b-2':''} text-center block w-full`} onClick={(e)=> rotationFunc('certificates',e)} href="">Certificates</a>
           </div>
 
         </div>
 
         <div className={`${st.projects} projects-div overflow-hidden relative z-50`}>
-          <a className='text-center block w-full' onClick={(e)=> rotationFunc('projects',e)} href="">Projects</a>
+          <a className={`${rotateval == 'projects-rotate-basic'? 'border-b-2':''} text-center block w-full`} onClick={(e)=> rotationFunc('projects',e)} href="">Projects</a>
         </div>
       </div>
       
